307be2b7c19389cd821d48951a8d1b0b30bee3c5
NayeemH <nayeemh67@gmail.com>
2021-01-11 10:07:37 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rangpur Vaccine</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit dolores iusto perspiciatis ab, laborum eaque molestiae maxime quidem quis rem fuga dolor nulla necessitatibus? Aliquam at ea laborum sint dolorum!</p>
+</body>
+</html>
