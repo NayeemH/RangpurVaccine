ecfbed1dc80ac9a2ea9de45b82c8b1c42a67ff60
NayeemH <nayeemh67@gmail.com>
2021-01-14 20:19:27 +0600

    basic form done

--- index.php
+++ index.php
@@ -13,6 +13,24 @@
         href='https://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.0/css/bootstrapValidator.min.css'>
     <link rel="stylesheet" href="./style.css">
 
+    <script>
+    function showHideCovid() {
+        if (document.getElementById('covidTestBeforeYes').checked) {
+            document.getElementById('covidTestDate').style.display = 'block';
+        } else {
+            document.getElementById('covidTestDate').style.display = 'none';
+        }
+    }
+
+    function showHideImmunity() {
+        if (document.getElementById('immunityYes').checked) {
+            document.getElementById('immunityTimes').style.display = 'block';
+        } else {
+            document.getElementById('immunityTimes').style.display = 'none';
+        }
+    }
+    </script>
+
 </head>
 
 <body>
@@ -82,7 +100,7 @@
                     <div class="col-md-4 inputGroupContainer">
                         <div class="input-group">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input name="last_name" placeholder="Last Name" class="form-control" type="date">
+                            <input name="last_name" placeholder="Last Name" class="form-control" type="text">
                         </div>
                     </div>
                 </div>
@@ -94,7 +112,23 @@
                     <div class="col-md-4 inputGroupContainer">
                         <div class="input-group">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input name="last_name" placeholder="Last Name" class="form-control" type="text">
+                            <input name="last_name" placeholder="Last Name" class="form-control" type="date">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- radio input -->
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">Gender</label>
+                    <div class="col-md-4 inputGroupContainer">
+                        <div class="input-group">
+                            <input type="radio" id="male" name="gender" value="male">
+                            <label for="male"> Male</label><br>
+                            <input type="radio" id="female" name="gender" value="female">
+                            <label for="female"> Female</label><br>
+                            <input type="radio" id="other" name="gender" value="other">
+                            <label for="other"> Other</label>
                         </div>
                     </div>
                 </div>
@@ -102,7 +136,7 @@
                 <!-- Text input-->
 
                 <div class="form-group">
-                    <label class="col-md-4 control-label">Username</label>
+                    <label class="col-md-4 control-label">Client Name</label>
                     <div class="col-md-4 inputGroupContainer">
                         <div class="input-group">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
@@ -111,51 +145,150 @@
                     </div>
                 </div>
 
-                <!-- Text input-->
-
+                <!-- Dropdown input -->
                 <div class="form-group">
-                    <label class="col-md-4 control-label">Password</label>
-                    <div class="col-md-4 inputGroupContainer">
+                    <label class="col-md-4 control-label">Profession</label>
+                    <div class="col-md-4 selectContainer">
                         <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input name="user_password" placeholder="Password" class="form-control" type="password">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
+                            <select name="department" class="form-control selectpicker">
+                                <option value="">Select your Department/Office</option>
+                                <option>Department of Engineering</option>
+                                <option>Department of Agriculture</option>
+                                <option>Accounting Office</option>
+                                <option>Tresurer's Office</option>
+                                <option>MPDC</option>
+                                <option>MCTC</option>
+                                <option>MCR</option>
+                                <option>Mayor's Office</option>
+                                <option>Tourism Office</option>
+                            </select>
                         </div>
                     </div>
                 </div>
 
-                <!-- Text input-->
+                <!-- number input-->
 
                 <div class="form-group">
-                    <label class="col-md-4 control-label">Confirm Password</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input name="confirm_password" placeholder="Confirm Password" class="form-control"
-                                type="password">
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Text input-->
-                <div class="form-group">
-                    <label class="col-md-4 control-label">E-Mail</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                            <input name="email" placeholder="E-Mail Address" class="form-control" type="text">
-                        </div>
-                    </div>
-                </div>
-
-
-                <!-- Text input-->
-
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Contact No.</label>
+                    <label class="col-md-4 control-label">Mobile Number</label>
                     <div class="col-md-4 inputGroupContainer">
                         <div class="input-group">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
-                            <input name="contact_no" placeholder="(639)" class="form-control" type="text">
+                            <input name="contact_no" placeholder="(639)" class="form-control" type="number">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- number input-->
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">NID</label>
+                    <div class="col-md-4 inputGroupContainer">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+                            <input name="contact_no" placeholder="(639)" class="form-control" type="number">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- number input-->
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">Birth Certificate NO</label>
+                    <div class="col-md-4 inputGroupContainer">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+                            <input name="contact_no" placeholder="(639)" class="form-control" type="number">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- number input-->
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">Emergency Contact Number</label>
+                    <div class="col-md-4 inputGroupContainer">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+                            <input name="contact_no" placeholder="(639)" class="form-control" type="number">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Checkbox input-->
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">Did you COVID-19 Test Before?</label>
+                    <div class="col-md-4 inputGroupContainer">
+                        <div class="input-group">
+                            <input type="checkbox" id="covidTestBeforeYes" name="covidTestBeforeYes" value="yes"
+                                onclick="showHideCovid()">
+                            <label for="covidTestBeforeYes"> Yes</label><br>
+                            <input type="checkbox" id="covidTestBeforeNo" name="covidTestBeforeNo" value="no">
+                            <label for="covidTestBeforeNo"> No</label><br>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Date input-->
+
+                <div class="form-group" id="covidTestDate">
+                    <label class="col-md-4 control-label">In which Date?</label>
+                    <div class="col-md-4 inputGroupContainer">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input name="last_name" placeholder="Last Name" class="form-control" type="date">
+                        </div>
+                    </div>
+                </div>
+
+
+                <!-- Checkbox input-->
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">Immunity Compromised Patient?</label>
+                    <div class="col-md-4 inputGroupContainer">
+                        <div class="input-group">
+                            <input type="checkbox" id="immunityYes" name="immunityYes" value="yes"
+                                onclick="showHideImmunity()">
+                            <label for="immunityYes"> Yes</label><br>
+                            <input type="checkbox" id="immunityNo" name="immunityNo" value="no">
+                            <label for="immunityNo"> No</label><br>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Date input-->
+
+                <div class="form-group" id="immunityTimes">
+                    <label class="col-md-4 control-label">How Many Times?</label>
+                    <div class="col-md-4 inputGroupContainer">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input name="last_name" placeholder="Last Name" class="form-control" type="text">
+                        </div>
+                    </div>
+                </div>
+
+
+                <!-- Dropdown input -->
+                <div class="form-group">
+                    <label class="col-md-4 control-label">Profession</label>
+                    <div class="col-md-4 selectContainer">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
+                            <select name="department" class="form-control selectpicker">
+                                <option value="">Select your Department/Office</option>
+                                <option>Department of Engineering</option>
+                                <option>Department of Agriculture</option>
+                                <option>Accounting Office</option>
+                                <option>Tresurer's Office</option>
+                                <option>MPDC</option>
+                                <option>MCTC</option>
+                                <option>MCR</option>
+                                <option>Mayor's Office</option>
+                                <option>Tourism Office</option>
+                            </select>
                         </div>
                     </div>
                 </div>
